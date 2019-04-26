cf8c2f5a018ae96c3704616156662247eae0ad3d
Enah <182255@stud.hvl.no>
2019-04-26 12:06:09 +0200

    TODO: fix put

--- ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
@@ -52,69 +52,67 @@ public class App {
 //		});
 
 		// PATH
-		path("/accessdevice", () -> {			
+		path("/accessdevice", () -> {
 			post("/log/", (req, res) -> {
+				res.type("application/json");
 				String message = req.body();
 				Gson gson = new Gson();
 				accesslog.add(message);
-				
+
 				return req.body();
-				
+
 			});
-			
+
 			get("/log/", (req, res) -> {
 				res.type("application/json");
 				Gson gson = new Gson();
-				
+
 //				for(int i )
 
 				return gson.toJsonTree(accesslog.log);
 			});
-						
-			get("/log/{id}", (req, res) -> {
+
+			get("/log/:id", (req, res) -> {
 				res.type("application/json");
 				int id = -1;
 				Gson gson = new Gson();
 				try {
-					id = Integer.parseInt(req.params("{id}"));
+					id = Integer.parseInt(req.params(":id"));
 					return gson.toJsonTree(accesslog.get(id));
-				} catch(Exception e) {
+				} catch (Exception e) {
 					return gson.toJsonTree("FAIL");
-					
-				} 
-				
-				
-				
-			});
 
-			get("/code", (req, res) -> {
-				Gson gson = new Gson();
-				String a = String.valueOf(accesscode.getAccesscode()[0]);
-				String b = String.valueOf(accesscode.getAccesscode()[1]);
+				}
 
-				// TODO: fix this shit
-				String json = "accesscode : ["  + a + ", " + b + "]";
-
-				return gson.toJson(json);
 			});
 
 			delete("/log/", (req, res) -> {
 				accesslog.clear();
 				Gson gson = new Gson();
+
 				return gson.toJson(accesslog.toJson());
 			});
 
 			put("/code", (req, res) -> {
+				res.type("application/json");
 				Gson gson = new Gson();
-				
-				AccessCode ac = gson.fromJson(req.body(), AccessCode.class);
+
+				try{AccessCode ac = gson.fromJson(req.body(), AccessCode.class);
 				accesscode.setAccesscode(ac.getAccesscode());
-				
+				} catch (Exception e) {
+					System.out.println("fail");
+				}
+
 				return req.body();
-			
 			});
 
-		}); // end path  
+			get("/code", (req, res) -> {
+				Gson gson = new Gson();
+
+				return gson.toJson(accesscode);
+			});
+
+		}); // end path
 
 	}
 }
